a9b1e655a2253d46c75070a08c57e3a8ae5f62fb
kumarprincepk <princekumar35071@gmail.com>
2025-06-30 00:30:17 +0530

    my first pattern code

---
+++ pattern1.js
@@ -0,0 +1,12 @@
+function sqrPat(n){
+  for(let i = 0; i<n; i++){
+    let row = "";
+    for(let j = 0; j<n;j++){
+      row += "* "
+    }
+    console.log(row)
+  }
+}
+
+let pattern = sqrPat(5);
+console.log(pattern)
