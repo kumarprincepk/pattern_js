6fdd0361168c298f7482c208c277045136231afb
kumarprincepk <princekumar35071@gmail.com>
2025-07-04 23:44:34 +0530

    update pattern 4

--- pattern4.js
+++ pattern4.js
@@ -1,11 +1,13 @@
-function trangPat(n){
-    for(let row = 0; row<n; row++){
-        let star = "";
-        for(let col = n-row; col>0; col--){
-            star += "* ";
-        }
-        console.log(star)
+function trangPat(n) {
+  for (let row = 0; row < n; row++) {
+    let star = "";
+    for (let col = n - row; col > 0; col--) {
+      if (row > col - n) {
+        star += "* ";
+      }
     }
+    console.log(star);
+  }
 }
 
-trangPat(5)
+trangPat(5);
