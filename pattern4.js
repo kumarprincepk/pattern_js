6f5ea03c863d80f993d327f273b8e6035251fc79
kumarprincepk <princekumar35071@gmail.com>
2025-07-05 23:10:47 +0530

    add

--- pattern4.js
+++ pattern4.js
@@ -2,9 +2,7 @@ function trangPat(n) {
   for (let row = 0; row < n; row++) {
     let star = "";
     for (let col = n - row; col > 0; col--) {
-      if (row > col - n) {
         star += "* ";
-      }
     }
     console.log(star);
   }
