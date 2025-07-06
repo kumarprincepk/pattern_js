f18b224e0a63e2541baf56e22b9ebc355a7e60ad
kumarprincepk <princekumar35071@gmail.com>
2025-07-06 23:42:46 +0530

    complete pat 4

--- pattern4.js
+++ pattern4.js
@@ -1,10 +1,8 @@
 function trangPat(n) {
   for (let row = 0; row < n; row++) {
-    let star = "";
-    for (let col = n - row; col > 0; col--) {
-        star += "* ";
-    }
-    console.log(star);
+    let spaces = " ".repeat(row);
+    let stars = "*".repeat(n - row);
+    console.log(spaces + stars);
   }
 }
 
